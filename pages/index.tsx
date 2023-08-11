89c4081e11111067e3390f4f492088aac5e727d8
madaley1 <mor2daley@gmail.com>
2023-08-11 09:48:14 -0700

    update paths to reflect aliases

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { GetStaticProps } from "next"
-import Layout from "../components/Layout"
-import Post, { PostProps } from "../components/Post"
+import Layout from "@/Components/Layout"
+import Post, { PostProps } from "@/Components/Post"
 
 export const getStaticProps: GetStaticProps = async () => {
   const feed = [
