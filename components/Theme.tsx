1f4538487e92244201c0989ea7f25d7c5f6045f4
Long Huynh <longhuynh.y2k@gmail.com>
2023-08-13 16:13:06 -0400

    Code review Navbar organization

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -1,8 +1,6 @@
 import { createTheme } from '@mui/material/styles';
 import '@fontsource/work-sans';
 
-
-// Define your custom theme
 const theme = createTheme({
   palette: {
     primary: {
@@ -12,7 +10,6 @@ const theme = createTheme({
   typography: {
     fontFamily: '"Work Sans", sans-serif',
   },
-  // Other theme properties (spacing, breakpoints, etc.)
 });
 
 export default theme;
---
+++ src/utils/constants.tsx
@@ -0,0 +1,9 @@
+// TODO: pull from DB/API
+export const PAGES = [
+    { name: 'Book Catalog', link: 'books' },
+    { name: 'Members', link: 'members' },
+    { name: 'Cafe Schedule', link: 'schedule' },
+    { name: 'Staff', link: 'staff' }
+  ];
+
+export const SETTINGS = ['Profile', 'Account', 'Dashboard', 'Logout'];
