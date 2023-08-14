08faf44667047df37db03e5913a06bc5dece68b4
mcorreira <meganmcorreira@gmail.com>
2023-08-14 00:48:44 -0400

    pages created, correspond to navbar

---
+++ pages/books.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { GetStaticProps } from "next";
+import Layout from "@/components/Layout";
+
+const Catalog = () => {
+  return (
+    <Layout>
+      <div className="page">
+        <h1>Book Catalog</h1>
+        <div className="post">{/* Content of each book post */}</div>
+      </div>
+      <style jsx>{`
+        .post {
+          background: white;
+          transition: box-shadow 0.1s ease-in;
+          /* Other styling properties */
+        }
+
+        .post:hover {
+          box-shadow: 1px 1px 3px #aaa;
+        }
+
+        .post + .post {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default Catalog;
---
+++ pages/schedule.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { GetStaticProps } from "next";
+import Layout from "@/components/Layout";
+
+const Schedule = () => {
+  return (
+    <Layout>
+      <div className="page">
+        <h1>Cafe Schedule</h1>
+      </div>
+      <style jsx>{`
+        .post {
+          background: white;
+          transition: box-shadow 0.1s ease-in;
+          /* Other styling properties */
+        }
+
+        .post:hover {
+          box-shadow: 1px 1px 3px #aaa;
+        }
+
+        .post + .post {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default Schedule;
