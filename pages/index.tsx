f8def70deb46c0b72b5b2ad9f64f45c70e4c79ad
MichaelShingo <mcrawford5376@gmail.com>
2023-08-17 10:18:39 +0200

    search component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import { GetStaticProps } from "next"
 import Layout from "@/components/Layout"
 import Table from "../components/Table";
-
+import Search from "@/components/Search";
 export const getStaticProps: GetStaticProps = async () => {
   return { 
     props: {}, 
@@ -10,10 +10,17 @@ export const getStaticProps: GetStaticProps = async () => {
 }
 
 const Blog: React.FC = () => {
+  const [search, setSearch] = useState<string>('');
+
   return (
     <Layout>
       <div className="page">
         <h1>Book Catalog</h1>
+        <Search 
+          search={search}
+          setSearch={setSearch}
+        />
+
         <Table />
       </div>
       <style jsx>{`
