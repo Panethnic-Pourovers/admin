25efcbdbc0b14c7e327abd6d3cad2dafa6b4d712
madaley1 <mor2daley@gmail.com>
2023-08-21 12:25:45 -0700

    update theme file with boxShadow

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -17,7 +17,8 @@ const theme = createTheme({
     MuiButton: {
       styleOverrides: {
         root: {
-          margin: '1em'
+          margin: '1em',
+          boxShadow: "0px 3px 1px -2px rgba(0, 0, 0, 0.2)"
         }
       }
     }
