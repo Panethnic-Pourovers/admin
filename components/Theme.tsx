b63086f2f5a45f8bf1e1475f71237cbdab32020a
Long Huynh <longhuynh.y2k@gmail.com>
2023-08-23 16:53:03 -0400

    Added Checkin/Checkout Modal

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -18,7 +18,7 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           margin: '1em',
-          boxShadow: "0px 3px 1px -2px rgba(0, 0, 0, 0.2)"
+          padding: '6px 10px',
         }
       }
     }
