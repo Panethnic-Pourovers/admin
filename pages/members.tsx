f06463f281aa6d8bed86cf6005b7f5cf74f9c4bd
Long Huynh <longhuynh.y2k@gmail.com>
2023-09-14 22:26:07 -0400

    Added members table

--- pages/members.tsx
+++ pages/members.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+// React imports
+import React, { useState } from 'react';
+
+// MUI components
+import { Box, Button } from '@mui/material';
+
+// custom components
 import Layout from '@/components/Layout';
+import Table from '@/components/Table';
+import Search from '@/components/Search';
+
+import { GridColDef } from '@mui/x-data-grid';
+
+// dummy data import
+import jsonData from 'dummyDataMembers.json';
 
 const Members = () => {
+  //state
+  const [searchValue, setSearch] = useState('');
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'UUID', width: 100, flex: 1 },
+    { field: 'firstName', headerName: 'First Name', width: 300 },
+    { field: 'lastName', headerName: 'Last Name', width: 200 },
+    { field: 'email', headerName: 'Email', width: 200 },
+    { field: 'phone', headerName: 'Phone Number', width: 200 },
+    { field: 'checkouts', headerName: 'Checkouts', width: 200 },
+  ];
+
   return (
     <Layout>
-      <div className="page">{/* <h1>Members</h1> */}</div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-          /* Other styling properties */
-        }
-
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
+      <div>
+        <Box
+          sx={{
+            display: 'flex',
+            flexFlow: 'row nowrap',
+            justifyContent: 'space-between',
+          }}
+          className="bookCatalog-topbar"
+        >
+          <div className="bookCatalog-topbar-search">
+            <Search search={searchValue} setSearch={setSearch} />
+          </div>
+        </Box>
+        <Table rows={jsonData || []} columns={columns} />
+        <div style={{ textAlign: 'right' }}>
+          <Button variant="contained">Add Member</Button>
+        </div>
+      </div>
     </Layout>
   );
 };
