6298fa360f7bd1bd41dd0345589f36d02d4974bc
madaley1 <mor2daley@gmail.com>
2023-09-29 10:58:31 -0700

    partial commit

--- pages/api/books/POST.ts
+++ pages/api/books/POST.ts
@@ -1,8 +1,28 @@
 import prisma from '@/prisma/prisma';
 import { NextApiRequest } from 'next';
 
+const dummyBook = {
+  title: 'titleValue',
+  author: 'authorValue',
+  genre: 'genreValue',
+  region: 'regionValue',
+  regionTwo: 'regionTwoValue',
+};
+
+type Book = {
+  barcodeID: string;
+  title: string;
+  author: string;
+  genres: string[];
+  regions: string[];
+  location: string;
+  members: string[];
+};
+
 export default async function postHandler(req: NextApiRequest) {
-  console.log('POST');
   console.log(req.body);
+  const { body } = req;
+
+  await prisma.book.create({ data: req.body });
   return { message: 'POST' };
 }
