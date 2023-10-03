b2b286ede87d92b9a927c3ed9a53262ecd5a67ba
MichaelShingo <mcrawford5376@gmail.com>
2023-10-03 20:37:01 +0800

    moved id endpoints to [id]

--- pages/api/members/DELETE.ts
+++ pages/api/members/DELETE.ts
@@ -1,10 +1,20 @@
 import prisma from '@/prisma/prisma';
 
-export default async function deleteHandler(id: string) {
-  const deletedMember = await prisma.libraryMember.delete({
-    where: {
-      id,
-    },
-  });
-  return deletedMember;
+export default async function deleteHandler(id?: string) {
+  let deletedMember;
+  if (id) {
+    try {
+      deletedMember = await prisma.libraryMember.delete({
+        where: {
+          id,
+        },
+      });
+      return deletedMember;
+    } catch {
+      return;
+    }
+  } else {
+    await prisma.libraryMember.deleteMany({});
+    return { message: 'All members were successfully deleted.' };
+  }
 }
