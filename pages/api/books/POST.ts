a838f80b560e9a35354d0793a6730e3f7bf3acf3
MichaelShingo <mcrawford5376@gmail.com>
2023-10-24 21:21:23 +0800

    post creates genres and regions

--- pages/api/books/POST.ts
+++ pages/api/books/POST.ts
@@ -48,31 +48,71 @@ export default async function postHandler(req: NextApiRequest) {
         'Invalid book, please check the book object and resend after correcting the data',
     };
   }
-  console.log('here we are');
-  const { title, author, genres, regions } = body;
-  console.log(title, author);
+  const { title, author, genres, regions, location } = body;
 
   const formattedGenres = genres.map((str) => ({ id: str }));
   const formattedRegions = regions.map((str) => ({ id: str }));
+  const getLocation = await prisma.location.findFirst({
+    where: { name: 'PEPO Checkin' },
+  });
 
   const bookToSend = {
     title: title,
     author: author,
     checkedOut: false,
-    lastCheckedOut: null,
-    locationId: '',
-    genres: { connect: formattedGenres },
+    lastCheckedOut: new Date(),
+    location: {
+      connect: {
+        where: { id: getLocation.id },
+      },
+    },
+    genres: {
+      connectOrCreate: {
+        where: formattedGenres,
+        create: formattedGenres,
+      },
+    },
     regions: { connect: formattedRegions },
   };
-  const getLocation = await prisma.location.findFirst({
-    where: { name: 'PEPO Checkin' },
-  });
 
-  bookToSend.lastCheckedOut = new Date();
-  bookToSend.locationId = getLocation.id as string;
+  // create genres and regions if does not exist
+  for (const genre of genres) {
+    const res = await prisma.genre.findUnique({ where: { name: genre } });
+    if (!res) {
+      await prisma.genre.create({ data: { name: genre } });
+    }
+  }
+
+  for (const region of regions) {
+    const res = await prisma.region.findUnique({ where: { name: region } });
+    if (!res) {
+      await prisma.region.create({ data: { name: region } });
+    }
+  }
 
   await prisma.book.create({
-    data: bookToSend,
+    data: {
+      title: title,
+      author: author,
+      checkedOut: false,
+      lastCheckedOut: new Date(),
+      location: {
+        connectOrCreate: {
+          where: {
+            id: getLocation.id,
+          },
+          create: {
+            name: location,
+          },
+        },
+      },
+      genres: {
+        connect: genres.map((genreName) => ({ name: genreName })),
+      },
+      regions: {
+        connect: regions.map((regionName) => ({ name: regionName })),
+      },
+    },
     include: {
       genres: true,
       regions: true,
